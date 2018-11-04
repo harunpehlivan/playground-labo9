fd9e608b5d66d824adbc476aa65f6ac97b00595f
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-04 21:38:31 +0100

    prime

--- prime/src/Exercise3/test_prime.c
+++ prime/src/Exercise3/test_prime.c
@@ -4,37 +4,33 @@
 
 int _is_prime(int nb)
 {
-  int i, test;
-  test = 1;
+	int i, test;
+	test = 1;
 
- for (i = 2; i < nb; i++)
-    if (nb % i == 0) test = 0;
- return test;
-}
-
-int test_prime()
-{
-    int ok=0;
-    if(_is_prime(3) != is_prime(3))
-        ok=1;
-    return ok;
-    
+	for (i = 2; i < nb; i++)
+		if (nb % i == 0) test = 0;
+	return test;
 }
 
 int main(void) {
-	int ok=0;
-    //int prime_test[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    ok=test_prime();
-    if(!ok)
-    {
-        printf("TECHIO> message --channel \"TEST ERREUR\" 'Error function fail test '\n");
+	int i;
+	int prime_test[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int ok=1;
+	
+    for (i=0;i<20;i++) {		
+		if (_is_prime(prime_test[i]) != is_prime(prime_test[i])) {
+            ok = ok && 1;
+        }
+        else {
+            ok = ok && 0;
+            printf("TECHIO> message --channel \"TEST ERREUR\" 'Error function fail test (%d)'\n",i);
+        }
     }
+        
     if(ok)
         printf("TECHIO> success true\n");
     else  
 		printf("TECHIO> success false\n");
-  return 0;
+
+	return 0;
 }
-
-
-
